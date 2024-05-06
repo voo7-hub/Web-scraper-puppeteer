834ba3f8c66abd07994840cfad342dcd988c17af
Vishwas Gondaliya <vishu.gondaliya123@gmail.com>
2024-05-06 14:26:39 -0400

    puppeteer scraper example

---
+++ index.js
@@ -0,0 +1,24 @@
+import puppeteer from "puppeteer";
+
+const getQuotes = async () => {
+  // Start a Puppeteer session with:
+  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+  // - no default viewport (`defaultViewport: null` - website page will in full width and height)
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+
+  // Open a new page
+  const page = await browser.newPage();
+
+  // On this new page:
+  // - open the "http://quotes.toscrape.com/" website
+  // - wait until the dom content is loaded (HTML is ready)
+  await page.goto("http://quotes.toscrape.com/", {
+    waitUntil: "domcontentloaded",
+  });
+};
+
+// Start the scraping
+getQuotes();
